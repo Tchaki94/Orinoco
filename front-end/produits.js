ffe4e5f8d56aba14a4ca0efe93b32f1e0adf5675
guillaume L <mister941@msn.com>
2021-02-12 11:19:00 +0100

    mise a jour panier/produit

--- front-end/produits.js
+++ front-end/produits.js
@@ -3,33 +3,45 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams (queryString);
 
-//console.log(id);
 
 fetch("http://localhost:3000/api/furniture/" + urlParams.get("id"))
     .then(response => response.json())
     .then(furnitures => {
        if(furnitures._id === undefined){ throw new Error("L'article n'existe pas ")};
-        console.log(furnitures)
-        displayContainer(furnitures);
-        displaySelect(furnitures);
+        afficherContenair(furnitures);
+        afficherSelection(furnitures);
     }).catch(error => {
       console.log(error);
+      
       let container = document.getElementById('furnitureContainer');
       container.innerHTML = `<h1>Une erreur est survenue: ${error.message}</h1>`
-    })
+      
+  
+ });
 
-function displayContainer(furnitures){
+//function pour afficher produit selectionner
+
+function afficherContenair(furnitures){
   let container = document.getElementById('furnitureContainer');
   let article = `<article class="furnitureContainer"><div class="difElt"><div class="eltname"><h2>${furnitures.name}</h2></div>
   <div class="eltimg"><img class="imgShop" src="${furnitures.imageUrl}" alt="${furnitures.name}" ></div>
   <div class="eltdesc"><p>${furnitures.description}</p></div>
   <div class="choixvernis"></div>
   <div class="eltprice"><p>${furnitures.price + " €"}</p></div>
-  <div class="addtocart"><a class="add-cart" onclick="addItemToCart"><i class="fas fa-shopping-cart fa-1x"></i></a></div></div></article>`
+  <div class="addtocart"><a class="add-cart"><i class="fas fa-shopping-cart fa-1x"></i></a></div></div></article>`
   container.innerHTML += article;
-}
 
-function displaySelect(furnitures){
+  let btn = document.querySelector('.addtocart');
+  btn.addEventListener('click', () => {
+  alert('Vous avez ajouté ' + furnitures.name + ' à votre panier.')
+  ajoutLocalStorage();
+  prixTotal();
+})
+};
+
+//function pour afficher different vernis
+
+function afficherSelection(furnitures){
   var label = document.createElement('label');
   label.textContent = "Vernis : ";
   var type = document.createElement('select');
@@ -44,6 +56,39 @@ function displaySelect(furnitures){
   }
   label.appendChild(type);
   document.querySelector('.choixvernis').appendChild(label);
+};
+
+// function pour ajouter au localstorage
+
+function ajoutLocalStorage(){
+  
+  let panier = localStorage.getItem('panier');
+  panier = JSON.parse(panier);
+  furnitures = 0;
+
+  if (panier != null){
+
+      if(panier[furnitures.name] === undefined){
+        panier = {... panier, [furnitures.name] : furnitures}
+      } else {
+        panier = {[furnitures.name] : furnitures}
+      }
+      localStorage.setItem('panier', JSON.stringify(panier));
+  }
+};
+
+// function total si plusieur produit selectionné
+
+function prixTotal() {
+
+  let prix = parseInt(furnitures.price);
+  let prixPanier = json.parse(localStorage.getItem('prixTotal'));
+
+  if(prixDuPanier != null){
+    localStorage.setItem("prixTotal", prixDuPanier + price);
+  } else {
+    localStorage.setItem("prixTotal", price);
+  }
 }
-// rajouter le choix du vernis 
+
 
